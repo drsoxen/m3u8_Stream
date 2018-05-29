0addfab2736e769af36146f016fa28182422d5da
Chris Ziraldo <chris.ziraldo@youi.tv>
2018-05-29 13:59:12 -0400

    clearing out old live broadcast data when starting

--- app.js
+++ app.js
@@ -43,28 +43,37 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-function callback() { }
-
-
 //slow the livestream down https://www.ffmpeg.org/ffmpeg-formats.html#toc-Options-5
 app.get('/startStream', function (req, res) {
+
+  fs.readdir(currentDirectoy, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(currentDirectoy, file), err => {
+      if (err) throw err;
+    });
+  }
+});
+
+  console.log('live stream has started')
 
   ffmpegInstance = ffmpeg('./public/media/Sample_' + currentFile + '.mp4', { timeout: 432000 }).addOptions([
     '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
     '-level 3.0', 
     '-s 640x360',          // 640px width, 360px height output video dimensions
     '-start_number 0',     // start the first .ts segment at index 0
-    '-hls_time 10',        // 10 second segment duration
+    '-hls_time 2',         // 2 second segment duration
     '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
     '-f hls'               // HLS format
-  ]).output('./public/media/' + currentFormat + '/' + currentFile + '/index.m3u8').on('end', callback).run()
+  ]).output('./public/media/' + currentFormat + '/' + currentFile + '/index.m3u8').on('end', function() {console.log('live stream has completed')}).run()
     
     res.end();
 });
 
 app.get('/stopStream', function (req, res) {
   
-  ffmpegInstance.kill(); //https://github.com/fluent-ffmpeg/node-fluent-ffmpeg#killsignalsigkill-kill-any-running-ffmpeg-process
+  ffmpegInstance.kill('SIGSTOP'); //https://github.com/fluent-ffmpeg/node-fluent-ffmpeg#killsignalsigkill-kill-any-running-ffmpeg-process
   fs.appendFile('./public/media/'  + currentFormat + '/' + currentFile + '/index.m3u8', '#EXT-X-ENDLIST', function (err) {
 
 });
